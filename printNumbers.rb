11b8d6deafd30e821087b494224480d7ac3532a6
Lyle Hart III <lyle.hart@cellularsales.com>
2016-05-31 15:58:31 -0400

    I like Ruby

--- printNumbers.rb
+++ printNumbers.rb
@@ -1,6 +1,25 @@
 $arrayOfNumbers = Array.new(16) { |num| num = num + 1}
+
 def printArray (array)
-	array.select {|num| puts "#{num}, "}
+	array.each do |num| 
+		puts "#{num}"
+	end
 end
 
-printArray($arrayOfNumbers)
+def printSubArray (array, x, y)
+	printArray(array[x..y])
+end
+
+def printBatchesOfFour (array)
+	x = 0
+	y = 3
+	
+	while (x < array.length) do
+		puts "print 4 items"
+		printSubArray(array, x, y)
+		x = x + 4
+		y = y + 4
+	end
+end
+
+printBatchesOfFour($arrayOfNumbers)
